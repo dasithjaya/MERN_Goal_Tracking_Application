e8227a0919913de3da5a504f253f2e767b90e073
dasithjaya <2020cs081@stu.ucsc.cmb.ac.lk>
2023-08-18 23:28:25 +0530

    Added JWT authentications

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+const protect = asyncHandler(async (req, res, next) => {
+    let token
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            // Get token from Header
+            token = req.headers.authorization.split(' ')[1]
+            // In there 'Bearer' is the firts item after split in to array and token is the second item.
+            // So use [1] to get the token
+
+            // Verify token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+            // Get user from the token
+            req.user = await User.findById(decoded.id).select('-password')
+
+            next()
+        } catch (error) {
+            console.log(error)
+            res.status(401)
+            throw new Error('Not authorized')
+        }
+    }
+
+    if (!token) {
+        res.status(401)
+        throw new Error('Not authorized, no token');
+    }
+})
+
+module.exports = {protect}
